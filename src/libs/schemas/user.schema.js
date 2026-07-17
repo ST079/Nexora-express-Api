ce2c30b585085def702fd26b5060c407ce401961
Sujan Tamang <suzanyba079@gmail.com>
2026-07-17 10:17:06 +0545

    fix: Make address field optional in user schema definition

--- src/libs/schemas/user.schema.js
+++ src/libs/schemas/user.schema.js
@@ -6,12 +6,14 @@ const userSchema = z.object({
   email: z.email(),
   password: z.string().min(6, "Password must be at least 6 characters long."),
   phone: z.string().regex(/^\d{10}$/, "Phone must be 10 digits"),
-  address: z.object({
-    country: z.string(),
-    province: z.string(),
-    city: z.string(),
-    street: z.string().optional(),
-  }),
+  address: z
+    .object({
+      country: z.string(),
+      province: z.string(),
+      city: z.string(),
+      street: z.string(),
+    })
+    .optional(),
   roles: z.array(z.enum([ROLE_ADMIN, ROLE_MERCHANT, ROLE_USER])).optional(),
   profileImageUrl: z.string().optional(),
   isActive: z.boolean().default(true).optional(),
