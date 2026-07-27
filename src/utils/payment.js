eda5af9c294c2ae3f2344b82b617ad09497660c9
Sujan Tamang <suzanyba079@gmail.com>
2026-07-27 10:11:29 +0545

    paisa

--- src/utils/payment.js
+++ src/utils/payment.js
@@ -7,7 +7,7 @@ const payViaKhalti = async (data) => {
     const body = {
       return_url: `${config.app_url}/${config.return_url}`,
       website_url: config.app_url,
-      amount: data.amount, // Convert to paisa
+      amount: data.amount * 100, // Convert to paisa
       purchase_order_id: data.purchase_order_id,
       purchase_order_name: data.purchase_order_name,
       customer_info: {
@@ -16,7 +16,7 @@ const payViaKhalti = async (data) => {
         phone: data.customer_info.phone,
       },
     };
-   
+
     const response = await axios.post(config.khalti.api_url, body, {
       headers: {
         Authorization: "Key " + config.khalti.api_key,
